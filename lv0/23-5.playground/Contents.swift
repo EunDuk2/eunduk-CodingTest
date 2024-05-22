de1e93edbcef96491ffe106d0c04c16688b8f85c
eunduk2 <dmsqls6789@naver.com>
2024-05-22 21:34:35 +0900

    23-5 문제 해결

---
+++ lv0/23-5.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func solution(_ date1:[Int], _ date2:[Int]) -> Int {
+    let (year1, month1, day1) = (date1[0], date1[1], date1[2])
+    let (year2, month2, day2) = (date2[0], date2[1], date2[2])
+    
+    if year1 < year2 {
+        return 1
+    } else if year1 > year2 {
+        return 0
+    } else {
+        if month1 < month2 {
+            return 1
+        } else if month1 > month2 {
+            return 0
+        } else {
+            if day1 < day2 {
+                return 1
+            } else {
+                return 0
+            }
+        }
+    }
+}
