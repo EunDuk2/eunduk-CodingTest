1b74024fd7072d841c89a25f1f423549c55f49ca
eunduk2 <dmsqls6789@naver.com>
2024-06-25 23:24:19 +0900

    둘만의 암호 var -> let

--- lv1/둘만의 암호.playground/Contents.swift
+++ lv1/둘만의 암호.playground/Contents.swift
@@ -4,7 +4,7 @@ func solution(_ s:String, _ skip:String, _ index:Int) -> String {
     var result = ""
     var temp = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"].filter { !skip.contains($0) }
     let count = temp.count
-    var alphabet = temp.enumerated()
+    let alphabet = temp.enumerated()
     
     for chr in s {
         let i = alphabet.filter { $0.element == String(chr) }.first?.offset
